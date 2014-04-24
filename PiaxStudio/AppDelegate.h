00cbd89202adf1ce479177789495d23e03dd067c
Bobie <bobie@herxun.co>
2014-04-24 19:45:01 +0800

    Initial Commit

---
+++ PiaxStudio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PiaxStudio
+//
+//  Created by Bobie Chen on 2014/4/24.
+//  Copyright (c) 2014年 Bobie Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
